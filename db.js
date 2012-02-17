ca98ca882db1e5c757a4d4822e219b91c97d47e6
Niklas Hambüchen <mail@nh2.me>
2012-02-17 15:49:17 +0000

    Add backend

---
+++ db.js
@@ -0,0 +1,45 @@
+var http = require("http");
+var mongodb = require('mongodb');
+var express = require('express');
+
+
+var server = new mongodb.Server("127.0.0.1", 27017, {});
+
+new mongodb.Db('cv', server, {}).open(function (error, client) {
+  if (error) throw error;
+  var collection = new mongodb.Collection(client, 'cv_collection');
+  var app = express.createServer();
+
+  app.use(express.bodyParser());
+
+  app.get('/users/all', function(req, res, next){
+    collection.find().toArray(function(err, results) {
+      console.dir(results);
+      res.send(JSON.stringify(results));
+    });
+  });
+
+  app.get('/users/get/:id?', function(req, res, next){
+      var uid = req.params.id;
+      if (uid) {
+        console.log(uid);
+        collection.find({'uid': uid}, {limit: 1}).toArray(function(err, user) {
+          console.dir(user[0]);
+          res.send(JSON.stringify(user[0]));
+        });
+      } else {
+          next();
+      }
+  });
+
+  app.post('/users/put/', function(req, res) {
+
+    var obj = req.body;
+
+    collection.update({'uid': obj.uid}, obj, {upsert: true});
+
+    res.send(req.body);
+  });
+
+  app.listen(8888);
+});
