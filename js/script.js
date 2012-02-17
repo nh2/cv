5eca7075cde992c54118bc6cd68bbd6d3373926f
Will Morgan <me@willmorgan.co.uk>
2012-02-17 22:12:00 +0000

    Pretty images and stuff.

--- js/script.js
+++ js/script.js
@@ -1,65 +1,44 @@
-<<<<<<< HEAD
-/* Author:
-
-*/
-
-
-
-
-
-=======
 (function() {
 	var tree,
-		points = [];
+		points = [],
+		state = 0,
+		STATES = {
+			'Tree': 0,
+			'Info': 1,
+		},
+		typeColours = {
+			'Personal': '#fac6cb',
+			'Experience': '#d6cada',
+			'Education':  '#d9f2f6',
+			'Expertise': '#d93b78',
+			'Media': '#fbd1cc',
+			'Interests': '#c4e4e9',
+			'Aspirations': '#e5d6e5'
+		};
 
 	// Animation functions
 	function showInfo() {
+		state = STATES['Info'];
 		$('#Tree').animate({ top: '50%' }, 750);
 		$('#Info').fadeIn();
 	}
 
 	function showTree() {
+		state = STATES['Tree'];
 		$('#Tree').animate({ top: '0%' }, 750);
 		$('#Info').fadeOut();
 	}
 
-	$('#collapse').bind('click', function(ev) {
-		var link = $(this);
-		link.toggleClass('active');
-		if(link.hasClass('active')) {
-			showInfo();
-		}
-		else {
-			showTree();
-		}
-	});
+	function circlePath(x, y, r) {
+		var s = "M" + x + "," + (y-r) + "A"+r+","+r+",0,1,1,"+(x-0.1)+","+(y-r)+" z";
+		return s;
+	}
+
 	return (window.cv = {
 		init: function() {
 			var data = $.getJSON('js/someData.json');
 
 			tree = Raphael('Tree', 960, '100%');
-
-			tree.customAttributes.arc = function (xloc, yloc, value, total, R) {
-				var alpha = 360 / total * value,
-					a = (90 - alpha) * Math.PI / 180,
-					x = xloc + R * Math.cos(a),
-					y = yloc - R * Math.sin(a),
-					path;
-				if (total == value) {
-					path = [
-						["M", xloc, yloc - R],
-						["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
-					];
-				} else {
-					path = [
-						["M", xloc, yloc - R],
-						["A", R, R, 0, +(alpha > 180), 1, x, y]
-					];
-				}
-				return {
-					path: path
-				};
-			};
 
 			data.success(cv.main);
 
@@ -70,49 +49,69 @@
 				n = 25,
 				height = $('#Tree').height();
 			for(i in data.Employment) {
-				(function() {
+				(function(item) {
 					var circle;
-					circle = tree.path();
+					circle = tree.path(circlePath(480, height / 2, n));
 					n += 80;
 					circle.attr({
-						'fill': '#ff0',
-						'stroke-width': '1px',
-						'stroke': '#000',
-						'arc': [480, height / 2, 10, 10, n]
-					}).toBack();
+						'stroke-width': '6px',
+						'stroke': '#e5e5e5'
+					});
 
 					var test = 10,
-						angle = Math.PI * 2 / (test - 1),
-						dot;
+						angle = Math.PI * 2 / (test - 1);
 					while(--test) {
-						var x = Math.sin(angle * test) * (n + .5),
-							y = Math.cos(angle * test) * (n + .5);
+						(function() {
+						var dot,
+							x = Math.sin(angle * test) * (n + .5),
+							y = Math.cos(angle * test) * (n + .5),
+							normalColor = typeColours[item.Type],
+							hoverColor = Raphael.rgb2hsl(normalColor);
+
+						hoverColor.l -= 0.2;
+
+						hoverColor = Raphael.hsl2rgb(hoverColor);
+
 						x += 480;
 						y += height / 2;
-						dot = tree.circle(x, y, 5).attr('fill', '#00f').data('n', test);
+						dot = tree.circle(x, y, 14).attr({
+							'fill': typeColours[item.Type],
+							'stroke': 0
+						}).data('n', test);
 						$(dot.node).bind({
 							'click': function() {
-								alert('you clicked moi');
+								if(state == STATES['Tree']) {
+									showInfo();
+								}
+								$('#Info').text(item.Employer + ' ' + item.Responsibilities.join(' '));
+							},
+							'mouseover': function() {
+								dot.animate({ fill: hoverColor }, 200);
+								$(circle.node).trigger('mouseover');
+							},
+							'mouseout': function() {
+								dot.animate({ fill: normalColor }, 200);
+								$(circle.node).trigger('mouseout');
 							}
 						});
 						points.push(dot);
+						}());
 					}
 
 					$(circle.node).bind({
 						'mouseover': function(ev) {
 							circle.animate({
-								'fill': '#f00'
-							}, 200);
+								'stroke': '#ccc'
+							}, 200, '<>');
 						},
 						'mouseout': function(ev) {
 							circle.animate({
-								'fill': '#ff0'
-							}, 200);
+								'stroke': '#e5e5e5'
+							}, 200, '<>');
 						}
 					});
-				}());
+				}(data.Employment[i]));
 			}
 		}
 	});
 }());
->>>>>>> d04bd4617c7e33b0aa2e20871970c3bdb6b586c1
