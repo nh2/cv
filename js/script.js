9d00b434de2722f28356c3de676e80d94ba00feb
Will Morgan <me@willmorgan.co.uk>
2012-02-18 11:52:35 +0000

    Updated JS for filtering, etc etc. Some sample data added to JSON

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 (function() {
 	var tree,
+		treeStructure,
 		points = [],
 		state = 0,
 		STATES = {
@@ -12,8 +13,8 @@
 			'Education':  '#d9f2f6',
 			'Expertise': '#d93b78',
 			'Media': '#fbd1cc',
-			'Interests': '#c4e4e9',
-			'Aspirations': '#e5d6e5'
+			'Interest': '#c4e4e9',
+			'Aspiration': '#e5d6e5'
 		};
 
 	// Animation functions
@@ -40,6 +41,28 @@
 
 			tree = Raphael('Tree', 960, '100%');
 
+			tree.customAttributes.arc = function (xloc, yloc, value, total, R) {
+				var alpha = 360 / total * value,
+					a = (90 - alpha) * Math.PI / 180,
+					x = xloc + R * Math.cos(a),
+					y = yloc - R * Math.sin(a),
+					path;
+				if (total == value) {
+					path = [
+						["M", xloc, yloc - R],
+						["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
+					];
+				} else {
+					path = [
+						["M", xloc, yloc - R],
+						["A", R, R, 0, +(alpha > 180), 1, x, y]
+					];
+				}
+				return {
+					path: path
+				};
+			};
+
 			data.success(cv.main);
 
 		},
@@ -47,43 +70,94 @@
 			var i,
 				radius = 100,
 				n = 25,
-				height = $('#Tree').height();
-			for(i in data.Employment) {
-				(function(item) {
-					var circle;
-					circle = tree.path(circlePath(480, height / 2, n));
-					n += 80;
-					circle.attr({
-						'stroke-width': '6px',
-						'stroke': '#e5e5e5'
-					});
+				height = $('#Tree').height(),
+				years = {};
 
-					var test = 10,
-						angle = Math.PI * 2 / (test - 1);
-					while(--test) {
-						(function() {
+			treeStructure = data;
+
+			// Get different years from the structure
+			for(i in data.Cloud) {
+				var item = data.Cloud[i],
+					start = item.StartDate,
+					year = start.split('-')[0];
+
+				if(item.Type == 'Personal') {
+					year = -1;
+				}
+				else if(item.Type == 'Aspiration') {
+					year = Infinity;
+				}
+				else {
+					year = +year;
+				}
+
+				if(!years[year]) {
+					years[year] = [item]
+				}
+				else {
+					years[year].push(item);
+				}
+
+			}
+
+			// Sort years in a horrible fashion:
+			var sortArr = [], newYears = {};
+			for(i in years) {
+				sortArr.push(i);
+			}
+
+			sortArr = sortArr.sort(function(a, b) { return +a > +b; });
+
+			for(i in sortArr) {
+				(function(year) {
+					var circle;
+					circle = tree.path().toFront();
+					n += 44;
+					circle.attr({
+						'stroke-width': '8px',
+						'stroke': '#e5e5e5',
+						'arc': [480, height / 2, 10, 10, n]
+					});
+					if(sortArr[i] == -1) {
+						circle.attr({
+							'fill': '#ccc'
+						});
+					}
+					else if(sortArr[i] == Infinity) {
+						circle.attr({
+							'opacity': 0
+						});
+					}
+
+					var imgDim = 130;
+					tree.image('img/umadbro.png', 480 - imgDim / 2, height / 2 - imgDim / 2, imgDim, imgDim);
+
+					var angle = Math.PI * 2 / year.length,
+						k = 0, j;
+					for(j in year) {
+						(function(entry) {
 						var dot,
-							x = Math.sin(angle * test) * (n + .5),
-							y = Math.cos(angle * test) * (n + .5),
-							normalColor = typeColours[item.Type],
+							x = Math.sin(angle * k) * (n) + (480),
+							y = Math.cos(angle * k) * (n) + (height / 2),
+							normalColor = typeColours[entry.Type],
 							hoverColor = Raphael.rgb2hsl(normalColor);
 
-						hoverColor.l -= 0.2;
-
+						hoverColor.l -= 0.3;
 						hoverColor = Raphael.hsl2rgb(hoverColor);
 
-						x += 480;
-						y += height / 2;
+						// Create the dot
 						dot = tree.circle(x, y, 14).attr({
-							'fill': typeColours[item.Type],
+							'fill': typeColours[entry.Type],
 							'stroke': 0
-						}).data('n', test);
+						});
+
+						// Bind click, mouseover, mouseout etc.
 						$(dot.node).bind({
 							'click': function() {
 								if(state == STATES['Tree']) {
 									showInfo();
 								}
-								$('#Info').text(item.Employer + ' ' + item.Responsibilities.join(' '));
+								$('#Info').text(entry.Employer + ' ' + entry.Responsibilities.join(' '));
 							},
 							'mouseover': function() {
 								dot.animate({ fill: hoverColor }, 200);
@@ -94,8 +168,16 @@
 								$(circle.node).trigger('mouseout');
 							}
 						});
+
 						points.push(dot);
-						}());
+
+						dot.toFront();
+
+						year[j].Dot = dot;
+
+						++k;
+
+						}(year[j]));
 					}
 
 					$(circle.node).bind({
@@ -110,8 +192,66 @@
 							}, 200, '<>');
 						}
 					});
-				}(data.Employment[i]));
+				}(years[sortArr[i]]));
+
 			}
+
+			var activeFilter;
+
+			$('nav').delegate('a', 'click', function(ev) {
+				ev.preventDefault();
+
+				var i, link = $(this),
+					filtered = cv.filter(link.attr('data-column'), link.attr('data-filter'), { includeFalse: 1 }),
+					matched = filtered.match,
+					nomatch = filtered.nomatch;
+
+				for(i in matched) {
+					matched[i].Dot.animate({ opacity: 1 }, 250);
+				}
+				for(i in nomatch) {
+					nomatch[i].Dot.animate({ opacity: 0 }, 250);
+				}
+
+				if(activeFilter) {
+					activeFilter.removeClass('active');
+					if(activeFilter[0] == link[0]) {
+						for(i in treeStructure.Cloud) {
+							treeStructure.Cloud[i].Dot.animate({ opacity: 1 }, 250);
+						}
+						activeFilter = null;
+						return;
+					}
+				}
+
+				activeFilter = link.addClass('active');
+
+
+			});
+
+		},
+		filter: function(column, value, settings) {
+			settings = settings || {};
+			var i, item, results = [], nomatch = [];
+			for(i in treeStructure.Cloud) {
+				item = treeStructure.Cloud[i],
+				test = settings.invert ? item[column] != value : item[column] == value;
+
+				if(test) {
+					results.push(item);
+				}
+				else if(settings.includeFalse) {
+					nomatch.push(item);
+				}
+
+			}
+			if(settings.includeFalse) {
+				return {
+					match: results,
+					nomatch: nomatch
+				}
+			}
+			return results;
 		}
 	});
 }());
